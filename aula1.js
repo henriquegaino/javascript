03d31786bd5892edda99e9d3c95d1c3ef0a61bc3
Henrique Gaino <hmgaino@gmail.com>
2024-10-01 12:34:50 -0300

    Update

---
+++ aula1.js
@@ -0,0 +1,5 @@
+let nome = "Henrique";         // String
+let idade = 25;                // Number
+let casado = false;            // Boolean
+let habilidades = ["JavaScript", "HTML", "CSS"]; // Array
+let pessoa = {nome: "Henrique", idade: 25};  // Object
